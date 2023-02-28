690c5e86a17aa5226945f3ac04dcbdd490add1b3
AlexVlaso <alexvlas.javaprog@gmail.com>
2023-02-28 11:42:40 +0200

    Add new entity Country.java

---
+++ src/main/java/com/vlasov/projectshop/entity/Country.java
@@ -0,0 +1,18 @@
+package com.vlasov.projectshop.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name="country")
+@Data
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+    @Column(name="code")
+    private String code;
+    @Column(name="name")
+    private String name;
+}
